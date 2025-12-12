0b35c4d119bf09419458cbb70ece07f1e07b3de1
lou my1r <suharevegor0@gmail.com>
2025-12-12 14:30:24 +0300

    Added task1, 2

---
+++ Task1.cs
@@ -0,0 +1,41 @@
+﻿namespace PR19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>
+        {
+            "Сергей",
+            "Егор",
+            "Петр",
+            "Ольга",
+            "Семен",
+            "Семен"
+        };
+            names.Sort();
+
+            List<string> uniqueNames = RemoveDuplicates(names);
+
+            foreach (var name in uniqueNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+        static List<string> RemoveDuplicates(List<string> names)
+        {
+            List<string> uniqueNames = new List<string>();
+
+            foreach (var name in names)
+            {
+                if (!uniqueNames.Contains(name))
+                {
+                    uniqueNames.Add(name);
+                }
+            }
+
+            return uniqueNames;
+        }
+    }
+}
+
---
+++ Task2.cs
@@ -0,0 +1,37 @@
+﻿namespace PR19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string input = Console.ReadLine();
+
+            Dictionary<string, int> wordFrequency = CountWordFrequency(input);
+
+            foreach (var kvp in wordFrequency)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
+        }
+
+        static Dictionary<string, int> CountWordFrequency(string text)
+        {
+            string[] words = text.ToLower().Trim().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {                 
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount[word] = 1;
+                }
+            }
+            return wordCount;
+        }
+    }
+}
